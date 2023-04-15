b257924c5828661abf0a8cf98804ef62d3059f55
HuangYanhao0731 <1162554465@qq.com>
2023-04-15 21:42:00 +0800

    变更

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
 # from neo4j import GraphDatabase
-
-import sqlite3
+import jieba
+# import sqlite3
 from flask import jsonify
 from pathlib import Path
 import json
-
 import z
 import wt
 import cx
@@ -15,7 +14,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 from flask import send_from_directory
 
-# # 设置Neo4j连接
+# 设置Neo4j连接
 # uri = "bolt://localhost:7687"
 # user = "neo4j"
 # password = "123456"
@@ -23,43 +22,52 @@ from flask import send_from_directory
 
 
 # 初始化数据库
-def init_db():
-    if not Path("history.db").is_file():
-        conn = sqlite3.connect("history.db")
-        c = conn.cursor()
-        c.execute("""
-        CREATE TABLE history (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            question TEXT NOT NULL,
-            answer TEXT NOT NULL
-        )
-        """)
-        conn.commit()
-        conn.close()
-init_db()
+# def init_db():
+#     if not Path("history.db").is_file():
+#         conn = sqlite3.connect("history.db")
+#         c = conn.cursor()
+#         c.execute("""
+#         CREATE TABLE history (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             question TEXT NOT NULL,
+#             answer TEXT NOT NULL
+#         )
+#         """)
+#         conn.commit()
+#         conn.close()
+# init_db()
 
 # 配置文件上传
-pdfs = UploadSet('pdfs', DOCUMENTS)
-app.config['UPLOADED_PDFS_DEST'] = 'json/uploads'
-configure_uploads(app, pdfs)
+# pdfs = UploadSet('pdfs', DOCUMENTS)
+# app.config['UPLOADED_PDFS_DEST'] = 'json/uploads'
+# configure_uploads(app, pdfs)
 
 
 def process_question(question):
-    QW = cx.cx(question)
+    i = cx.fju(question)
+    p = []
+    answer = ''
+    print(i)
+    for question in i:
+        QW = cx.cx(question)
+        if QW in p:
+            continue
 
-    if QW in wt.wt:
-        answer = z.A[QW]
-    else:
-        answer = "暂无此内容"
+        if QW in wt.wt:
+            answer += z.A[QW]
+            answer += '\n'
+            p.append(QW)
+        else:
+            answer += "\n很抱歉,目前我暂时还无法回答你的问题！"
     # 生成回答
     answer = answer.strip()
-    # if answer:
-    #     # 将问题和答案存入数据库
-    #     conn = sqlite3.connect("history.db")
-    #     c = conn.cursor()
-    #     c.execute("INSERT INTO history (question, answer) VALUES (?, ?)", (question, answer))
-    #     conn.commit()
-    #     conn.close()
+    if answer:
+        # 将问题和答案存入数据库
+        conn = sqlite3.connect("history.db")
+        c = conn.cursor()
+        c.execute("INSERT INTO history (question, answer) VALUES (?, ?)", (question, answer))
+        conn.commit()
+        conn.close()
 
     return answer
 
@@ -74,28 +82,26 @@ def index():
     return render_template("index.html", answer=default_answer)
 
 # 历史记录
-@app.route("/history")
-def history():
-    # 从数据库中获取历史记录
-    conn = sqlite3.connect("history.db")
-    c = conn.cursor()
-    c.execute("SELECT question, answer FROM history")
-    records = c.fetchall()
-    conn.close()
-
-    return render_template("history.html", records=records)
+# @app.route("/history")
+# def history():
+#     # 从数据库中获取历史记录
+#     conn = sqlite3.connect("history.db")
+#     c = conn.cursor()
+#     c.execute("SELECT question, answer FROM history")
+#     records = c.fetchall()
+#     conn.close()
+#
+#     return render_template("history.html", records=records)
 
 # pdf上传
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST' and 'pdf' in request.files:
-        filename = pdfs.save(request.files['pdf'])
-        # return redirect(url_for('index'))
-    return render_template('upload.html')
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST' and 'pdf' in request.files:
+#         filename = pdfs.save(request.files['pdf'])
+#         # return redirect(url_for('index'))
+#     return render_template('upload.html')
 
-
-
-# # 添加一个新的路由来处理知识图谱的上传
+# 添加一个新的路由来处理知识图谱的上传
 # @app.route('/upload_kg', methods=['POST'])
 # def upload_kg():
 #     if 'file' not in request.files:
@@ -121,9 +127,8 @@ def upload():
 #             session.run(query)
 #
 #     return 'Knowledge graph uploaded and imported successfully', 200
-
 # 添加一个新的路由来显示知识图谱可视化
-#
+
 # @app.route("/api/get_kg_data")
 # def get_kg_data():
 #     query_nodes = "MATCH (n) RETURN n"
--- cx.py
+++ cx.py
@@ -5,6 +5,8 @@ def cx(name):
     jieba.add_word(r"舰艇损管")
     jieba.add_word(r"潜水方式")
     jieba.add_word(r"类")
+    if name[-1] == '？':
+        name = name[::-2]
     name_c = jieba.lcut(name)
     k = []
     p = 0
@@ -22,11 +24,26 @@ def cx(name):
         if i[1] > max:
             max = i[1]
             wz = i[0]
-    if max == 0:
-        ppp = '暂无此内容'
+    if max <= 0:
+        ppp = '很抱歉目前我暂时还无法回答你的问题！'
         return ppp
 
     return wt.wt[wz-1]
 
-
-
+def fju(name):
+    if name[-1] == '？':
+        name = name[:-2]
+    print(name)
+    L = []
+    m = ''
+    for i in name:
+        if i == '？':
+            m = m+i
+            L.append(m)
+            m = ''
+        else:
+            m = m+i
+    if m !='':
+        L.append(m)
+    print(L)
+    return L
